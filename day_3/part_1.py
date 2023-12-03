37321e8ae1464cfbffb12e05e2347aefe1089207
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2023-12-04 00:37:05 +0500

    Add solution for Day 3 (part 1)

---
+++ day_3/part_1.py
@@ -0,0 +1,160 @@
+import re
+from helper import parseFile
+
+FILENAME = "input.txt"
+
+SPECIAL_CHAR_REGEX = r"[^\.\d\s]"
+
+# def check_if_part_number(current_line, next_line, prev_line)
+
+DIRECTIONS = {
+    "NORTH": {"line_index": -1, "character_index": 0},
+    "SOUTH": {"line_index": 1, "character_index": 0},
+    "WEST": {"line_index": 0, "character_index": -1},
+    "EAST": {"line_index": 0, "character_index": 1},
+    "NORTHEAST": {"line_index": -1, "character_index": 1},
+    "NORTHWEST": {"line_index": -1, "character_index": -1},
+    "SOUTHEAST": {"line_index": 1, "character_index": 1},
+    "SOUTHWEST": {"line_index": 1, "character_index": -1},
+}
+
+
+def search_for_special_chars(lines, line_index, character_index):
+    special_char_flag_array = []
+
+    search_directions = None
+
+    # first line
+    if line_index == 0:
+        # first character
+        if character_index == 0:
+            search_directions = [
+                DIRECTIONS["EAST"],
+                DIRECTIONS["SOUTH"],
+                DIRECTIONS["SOUTHEAST"],
+            ]
+
+        # last character
+        elif character_index == len(lines[0]) - 1:
+            search_directions = [
+                DIRECTIONS["WEST"],
+                DIRECTIONS["SOUTH"],
+                DIRECTIONS["SOUTHWEST"],
+            ]
+
+        else:
+            search_directions = [
+                DIRECTIONS["WEST"],
+                DIRECTIONS["EAST"],
+                DIRECTIONS["SOUTH"],
+                DIRECTIONS["SOUTHEAST"],
+                DIRECTIONS["SOUTHWEST"],
+            ]
+
+    # last line
+    elif line_index == len(lines) - 1:
+        # first character
+        if character_index == 0:
+            search_directions = [
+                DIRECTIONS["NORTH"],
+                DIRECTIONS["EAST"],
+                DIRECTIONS["NORTHEAST"],
+            ]
+
+        # last character
+        elif character_index == len(lines[0]) - 1:
+            search_directions = [
+                DIRECTIONS["NORTH"],
+                DIRECTIONS["WEST"],
+                DIRECTIONS["NORTHWEST"],
+            ]
+
+        else:
+            search_directions = [
+                DIRECTIONS["NORTH"],
+                DIRECTIONS["WEST"],
+                DIRECTIONS["EAST"],
+                DIRECTIONS["NORTHEAST"],
+                DIRECTIONS["NORTHWEST"],
+            ]
+
+    else:
+        # first character
+        if character_index == 0:
+            search_directions = [
+                DIRECTIONS["NORTH"],
+                DIRECTIONS["SOUTH"],
+                DIRECTIONS["EAST"],
+                DIRECTIONS["NORTHEAST"],
+                DIRECTIONS["SOUTHEAST"],
+            ]
+
+        # last character
+        elif character_index == len(lines[0]) - 1:
+            search_directions = [
+                DIRECTIONS["NORTH"],
+                DIRECTIONS["SOUTH"],
+                DIRECTIONS["WEST"],
+                DIRECTIONS["NORTHWEST"],
+                DIRECTIONS["SOUTHWEST"],
+            ]
+
+        else:
+            search_directions = [value for value in DIRECTIONS.values()]
+
+    for direction in search_directions:
+        relative_line_index, relative_character_index = (
+            direction["line_index"],
+            direction["character_index"],
+        )
+
+        item = lines[relative_line_index + line_index][
+            relative_character_index + character_index
+        ]
+        search_result = re.search(SPECIAL_CHAR_REGEX, item)
+
+        if search_result is not None:
+            special_char_flag_array.append(True)
+        else:
+            special_char_flag_array.append(False)
+
+    return True in special_char_flag_array
+
+
+if __name__ == "__main__":
+    sum_of_part_numbers = 0
+    lines = parseFile(FILENAME)
+    part_numbers = []
+
+    for line_idx, line in enumerate(lines):
+        buffer = ""
+        is_part_number_array = []
+
+        for char_idx, char in enumerate(line):
+            if char.isdigit():
+                buffer += char
+
+                is_part_number = search_for_special_chars(
+                    lines=lines, line_index=line_idx, character_index=char_idx
+                )
+
+                is_part_number_array.append(is_part_number)
+
+                if (
+                    char_idx == len(line) - 1
+                    and len(buffer) > 0
+                    and True in is_part_number_array
+                ):
+                    part_numbers.append(int(buffer))
+
+            else:
+                if len(buffer) > 0 and True in is_part_number_array:
+                    part_numbers.append(int(buffer))
+
+                buffer = ""
+                is_part_number_array = []
+
+    for number in part_numbers:
+        sum_of_part_numbers += number
+
+    print("The sum of part numbers is:", sum_of_part_numbers)
